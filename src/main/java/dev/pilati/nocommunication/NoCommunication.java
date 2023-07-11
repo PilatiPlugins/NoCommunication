d41dd06544ffb536c4d2bfb0e32a6d03094525fe
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 22:44:15 -0300

    Ending 1.0 release

--- src/main/java/dev/pilati/nocommunication/NoCommunication.java
+++ src/main/java/dev/pilati/nocommunication/NoCommunication.java
@@ -1,5 +1,6 @@
 package dev.pilati.nocommunication;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev.pilati.nocommunication.listener.Book;
@@ -32,6 +33,8 @@ public class NoCommunication extends JavaPlugin {
         MessageManager.getInstance().load();
 
         registerEvents();
+
+        new Metrics(this, 19051);
     }
 
     @Override
@@ -41,30 +44,24 @@ public class NoCommunication extends JavaPlugin {
     }
 
     private void registerEvents() {
-        getLogger().info("Registering events");
         if (this.getConfig().getBoolean("deny-sign")) {
             this.getServer().getPluginManager().registerEvents(new Sign(), this);
-            getLogger().info("Blocking signs");
         }
 
         if (this.getConfig().getBoolean("deny-book")) {
             this.getServer().getPluginManager().registerEvents(new Book(), this);
-            getLogger().info("Blocking books");
         }
 
         if (this.getConfig().getBoolean("deny-item-rename")) {
             this.getServer().getPluginManager().registerEvents(new ItemRename(), this);
-            getLogger().info("Blocking item rename");
         }
 
         if (this.getConfig().getBoolean("deny-chat-message")) {
             this.getServer().getPluginManager().registerEvents(new ChatMessage(), this);
-            getLogger().info("Blocking chat message");
         }
 
         if (this.getConfig().getBoolean("deny-chat-command")) {
             this.getServer().getPluginManager().registerEvents(new ChatCommand(), this);
-            getLogger().info("Blocking chat command");
         }
     }
 
